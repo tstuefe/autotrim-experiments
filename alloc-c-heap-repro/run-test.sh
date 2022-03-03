5aeb05c664bfcd1b705ac0bfdd5ad3270ed08bf6
tstuefe <thomas.stuefe@gmail.com>
2022-03-03 12:03:04 +0100

    init

---
+++ alloc-c-heap-repro/run-test.sh
@@ -0,0 +1,11 @@
+JVM_OPTIONS="-Xmx256m -Xms256m -XX:+UnlockDiagnosticVMOptions -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:MonitorUsedDeflationThreshold=50 -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics"
+TEST_OPTIONS="--type=peak -y --cycles=50"
+
+TESTNAME=alloc-c-heap-mit-autotrim
+JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=10"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp ../../test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS 2>&1 | tee ${TESTNAME}-out.txt
+
+TESTNAME=alloc-c-heap-ohne-autotrim
+JVM_OPTIONS_HERE="-XX:-AutoTrimNativeHeap"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp ../../test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS 2>&1 | tee ${TESTNAME}-out.txt
+
---
+++ renaissance-phil-alwayspretouch/run-renaissance.sh
@@ -0,0 +1,11 @@
+JVM_OPTIONS="-Xmx256m -Xms256m -XX:+AlwaysPreTouch -XX:+UnlockDiagnosticVMOptions -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:MonitorUsedDeflationThreshold=50 -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics"
+BENCHMARK_OPTIONS="-r 50"
+
+TESTNAME=renaissance-mit-autotrim
+JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=10"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -jar ../../benchmarks/renaissance/renaissance-gpl-0.13.0.jar philosophers $BENCHMARK_OPTIONS 2>&1 | tee ${TESTNAME}-benchout.txt
+
+TESTNAME=renaissance-ohne-autotrim
+JVM_OPTIONS_HERE="-XX:-AutoTrimNativeHeap"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -jar ../../benchmarks/renaissance/renaissance-gpl-0.13.0.jar philosophers $BENCHMARK_OPTIONS 2>&1 | tee ${TESTNAME}-benchout.txt
+
