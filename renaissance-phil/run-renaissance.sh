d34d7d0040ad5e74793eb1af9bc00a60dd5f1fed
tstuefe <thomas.stuefe@gmail.com>
2022-03-04 14:34:39 +0100

    changes

--- renaissance-phil/run-renaissance.sh
+++ renaissance-phil/run-renaissance.sh
@@ -1,7 +1,7 @@
 JVM_OPTIONS="-Xmx256m -Xms256m -XX:+UnlockDiagnosticVMOptions -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=5 -XX:MonitorUsedDeflationThreshold=50 -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics"
 BENCHMARK_OPTIONS="-r 100"
 
-TESTNAME=renaissance-mit-autotrim-5s
+TESTNAME=renaissance-mit-autotrim-1s
 JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=1"
 ../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -jar ../../benchmarks/renaissance/renaissance-gpl-0.13.0.jar philosophers $BENCHMARK_OPTIONS 2>&1 | tee ${TESTNAME}-benchout.txt
 
