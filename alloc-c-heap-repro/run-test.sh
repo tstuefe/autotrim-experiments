8023a1b3ef0fcb0ccf4cfbaf0d7ac7e7c1d88e79
tstuefe <thomas.stuefe@gmail.com>
2022-03-07 11:47:48 +0100

    aloccheap-1-monday

--- alloc-c-heap-repro/run-test.sh
+++ alloc-c-heap-repro/run-test.sh
@@ -1,11 +1,21 @@
-JVM_OPTIONS="-Xmx1G -Xms1G -XX:+UnlockDiagnosticVMOptions -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:MonitorUsedDeflationThreshold=50 -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics"
-TEST_OPTIONS="--type=peak -y --cycles=5 --waitsecs=60"
+JVM_OPTIONS="-Xmx1G -Xms1G -XX:+AlwaysPreTouch -XX:+UnlockDiagnosticVMOptions -XX:+DumpVitalsAtExit -XX:VitalsSampleInterval=1 -XX:MonitorUsedDeflationThreshold=30 -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics"
+TEST_OPTIONS="--type=peak --allocdelay=1 --randfactor=3 --randseed=4040 -y --waitsecs=76"
 
-TESTNAME=alloc-c-heap-mit-autotrim
-JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=10"
-../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp ../../test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS 2>&1 | tee ${TESTNAME}-out.txt
 
-TESTNAME=alloc-c-heap-ohne-autotrim
+TESTNAME=baseline
 JVM_OPTIONS_HERE="-XX:-AutoTrimNativeHeap"
-../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp ../../test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS 2>&1 | tee ${TESTNAME}-out.txt
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp /shared/projects/openjdk/test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS | tee ${TESTNAME}-out.txt
+
+TESTNAME=autotrim-1s
+JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=1"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp /shared/projects/openjdk/test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS | tee ${TESTNAME}-out.txt
+
+TESTNAME=autotrim-30s
+JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=30"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp /shared/projects/openjdk/test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS | tee ${TESTNAME}-out.txt
+
+TESTNAME=autotrim-1s-adaptive
+JVM_OPTIONS_HERE="-XX:+AutoTrimNativeHeap -XX:AutoTrimNativeHeapInterval=1 -XX:+AutoTrimNativeHeapIntervalAdaptive"
+../../jdk-jdk/output-release/images/jdk/bin/java $JVM_OPTIONS $JVM_OPTIONS_HERE -XX:VitalsFile=$TESTNAME -cp /shared/projects/openjdk/test/repros/repros8/target/repros8-1.0.jar de.stuefe.repros.AllocCHeap $TEST_OPTIONS | tee ${TESTNAME}-out.txt
+
 
